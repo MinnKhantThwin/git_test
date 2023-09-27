a8ed7e24b55178b97a1a12f70e4ee3d852c5a2e0
MinnKhantThwin <minnkhanthwin@gmail.com>
2023-09-27 16:10:35 +0630

    remote test

--- src/main/MainSection.java
+++ src/main/MainSection.java
@@ -5,7 +5,8 @@ public class MainSection {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Inside main method...");
-		System.out.println("Inside main method");
+		System.out.println("Inside main method...");
+		System.out.println("Testing remote master");
 	}
 
 }
